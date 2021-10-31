d15dffb882e7c9149c83be06eb223c12cc7d030f
Raja Harshini Kasibhotla <rj751938@dal.ca>
2021-10-31 03:52:11 -0300

    added user interface

---
+++ src/main/java/com/csci5408/centdb/CentDBMain.java
@@ -0,0 +1,13 @@
+package com.csci5408.centdb;
+
+import java.io.IOException;
+
+public class CentDBMain {
+
+	public static void main(String args[]) throws IOException {
+
+		UserInterface userInterface = new UserInterface();
+		userInterface.userAccessControl();
+
+	}
+}
