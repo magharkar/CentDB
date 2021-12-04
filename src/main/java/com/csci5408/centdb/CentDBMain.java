9d544c8cfdab63c2391bb87c6326aaa1e45e2148
Sherrin Raji <sh810841@dal.ca>
2021-12-04 19:28:26 -0400

    corrections to user module

--- src/main/java/com/csci5408/centdb/CentDBMain.java
+++ src/main/java/com/csci5408/centdb/CentDBMain.java
@@ -1,11 +1,13 @@
 package com.csci5408.centdb;
 
+import com.csci5408.centdb.services.UserService;
+
 import java.io.IOException;
 public class CentDBMain {
 
 	public static void main(String[] args) throws IOException {
 
-		UserInterface userInterface = new UserInterface();
+		UserService userInterface = new UserService();
 		userInterface.userAccessControl();
 	}
 }
