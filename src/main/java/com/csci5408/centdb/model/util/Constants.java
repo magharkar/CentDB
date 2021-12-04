ef399508d858dad3864d4c2de31a1ad862f7f907
Sherrin Raji <sh810841@dal.ca>
2021-12-04 19:41:29 -0400

    Feature/encrypt user module

---
+++ src/main/java/com/csci5408/centdb/model/util/Constants.java
@@ -0,0 +1,11 @@
+package com.csci5408.centdb.model.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public final class Constants {
+    public static final String DELIMITER = "|";
+    public static final String USER_ID = "userId";
+    public static final List<String> securityQuestions = Arrays.asList("What elementary school did you attend?","Who was your childhood hero?");
+    
+}
