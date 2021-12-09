e0b454108ed5e5f37e1a2317a14ed6c243602697
Tanvi Pruthi <tn781812@dal.ca>
2021-12-08 21:58:24 -0400

    Update QueryLogs.java

--- src/main/java/com/csci5408/centdb/logging/QueryLogs.java
+++ src/main/java/com/csci5408/centdb/logging/QueryLogs.java
@@ -7,16 +7,16 @@ public class QueryLogs {
                                String table_name, String column_affected, String row_affected,
                                String constraint) throws IOException {
         String query_log="";
-        File f = new File("resources+\\Databases\\"+database_name+"\\"+database_name+"_QueryLogs.txt");
+        File f = new File("resources\\Databases\\"+database_name+"\\"+database_name+"_QueryLogs.txt");
         if(f.exists()) {
             BufferedReader br = new BufferedReader(new FileReader(f));
-//            if (br.readLine() == null) {
-//                query_log = "User|Query type|Status|Database Name|Table Name|Column Affected|Row Affected|Condition|TimeStamp\n";
-//            }
+            if (br.readLine() == null) {
+                query_log = "User|Query type|Status|Database Name|Table Name|Column Affected|Row Affected|Condition|TimeStamp\n";
+            }
         }
-//        else{
-//           // query_log = "User|Query type|Status|Database Name|Table Name|Column Affected|Row Affected|Condition|TimeStamp\n";
-//        }
+        else{
+            query_log = "User|Query type|Status|Database Name|Table Name|Column Affected|Row Affected|Condition|TimeStamp\n";
+        }
         query_log = query_log+userId+"|"+query_type+"|"+status+"|"+database_name+"|"+table_name+"|"+column_affected+"|"
                 +row_affected+"|"+constraint+"|"+(new Date());
         FileWriter writer = new FileWriter("resources\\Databases\\"+database_name+"\\"+database_name+"_QueryLogs.txt",true);
